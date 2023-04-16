e1495a45db4365c927be802d87aeeaed55f9c919
kieumylove <85643563+kieumylove@users.noreply.github.com>
2023-04-15 20:42:24 -0700

    search engine

--- src/app.js
+++ src/app.js
@@ -38,7 +38,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "6881ab061dc897e22c418a72954f13c4";
-let city = "New York";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(url).then(displayTemperature);
+function search(city) {
+  let apiKey = "6881ab061dc897e22c418a72954f13c4";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
